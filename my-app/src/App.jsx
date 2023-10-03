eef65bc5a303c98c18f92ab291529f71b7726a56
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-10-03 23:24:08 +0200

    message

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,15 +1,15 @@
-import Hello from './Hello';
+import Hello from "./Hello";
+import Message from "./Message";
 
 function App() {
   return (
     <div>
       <Hello />
-      <Hello />
+      <Message />
     </div>
   );
 }
 
 export default App;
 
-
-//using Hello twice, it is now rendered twice.
+//"You can use the Message in the App component by importing it. Its message will be displayed twice: once within the Hello component and once from the App component itself."
