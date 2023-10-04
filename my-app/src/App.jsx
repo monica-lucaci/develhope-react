c97fdec151774f33fcf485ad04d80b01798ce6f4
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-10-04 22:59:36 +0200

    risp prima domanda

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -17,5 +17,3 @@ const App = () => {
 };
 
 export default App;
-
-//"You can use the Message in the App component by importing it. Its message will be displayed twice: once within the Hello component and once from the App component itself."
--- my-app/src/Welcome.jsx
+++ my-app/src/Welcome.jsx
@@ -1,5 +1,7 @@
-const Welcome = ({ name = "guest"}) => {
+const Welcome = ({ name = "guest" }) => {
   return <p>Welcome, {name}!</p>;
 };
 
 export default Welcome;
+
+//se non passo il parametro stranamente non mi da errore,pero nessun nome viene visualizzato
