7ff2fe59764c5b848821e49538f03dabd3d7368a
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-10-05 19:11:44 +0200

    onclick added

--- my-app/src/MouseClicker.jsx
+++ my-app/src/MouseClicker.jsx
@@ -1,16 +1,35 @@
+import icon from "./icon.png";
+
 const MouseClicker = () => {
   const handleButtonClick = (event) => {
     console.log(event.currentTarget.name);
+  };
+
+  const handleImageClick = (event) => {
+    console.log(event.target.src);
   };
 
   return (
     <>
       <button name="one" onClick={handleButtonClick}>
         Click here!
-        <img width={24} height={24} />
+        <img
+          src={icon}
+          onClick={handleImageClick}
+          alt="Icon"
+          width={20}
+          height={20}
+        />
       </button>
     </>
   );
 };
 
 export default MouseClicker;
+
+//Since the image is contained within the <button> , the button's onClick event handler (handleButtonClick) is also triggered.
+//in order to stop the propagation this should be used
+// const handleImageClick = (event) => {
+//     console.log(event.target.src);
+//     event.stopPropagation();
+//   }
