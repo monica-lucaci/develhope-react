c1c87e52c3d28924e4fdd1a5477924719d1e24b8
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-10-05 09:57:41 +0200

    added prop

--- my-app/src/AlertClock.jsx
+++ my-app/src/AlertClock.jsx
@@ -1,11 +1,5 @@
-const AlertClock = () => {
-  const handleShowAlert = () => {
-    const time = new Date().toLocaleTimeString();
-
-    alert(`The time is ${time}`);
-  };
-
-  return <button onClick={handleShowAlert}>Show Current Time!</button>;
+const AlertClock = ({ onButtonClick }) => {
+  return <button onClick={onButtonClick}>Show Current Time!</button>;
 };
 
 export default AlertClock;
--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,20 +3,22 @@ import Hello from "./Hello";
 import Message from "./Message";
 import Welcome from "./Welcome";
 
-
 const App = () => {
   const age = 3;
+  const handleShowAlert = () => {
+    const time = new Date().toLocaleTimeString();
+
+    alert(`The time is ${time}`);
+  };
 
   return (
     <div>
       <Hello />
       <Message />
       <Welcome name="John" age={age} />
-      
-      <AlertClock />
+      <AlertClock onButtonClick={handleShowAlert} />
     </div>
   );
 };
 
 export default App;
-
