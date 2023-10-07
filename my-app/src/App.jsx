5f8a9ce65982b11709442ff2b5880223b1c07952
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-10-07 18:27:54 +0200

    Add a login button to the Login component

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,13 +1,13 @@
 import Login from "./Login";
 
-
+const handleLogin = (formData) => {
+  console.log("Logging in with data:", formData);
+};
 
 const App = () => {
-
-
   return (
     <div>
-      <Login />
+      <Login onLogin={handleLogin} />
     </div>
   );
 };
--- my-app/src/Login.jsx
+++ my-app/src/Login.jsx
@@ -1,41 +1,66 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-const CreateFormData = () => {return {
-    username: '',
-    password: '',
-    remember: '',
-} };
+const CreateFormData = () => {
+  return {
+    username: "",
+    password: "",
+    remember: false,
+  };
+};
 
+const Login = ({ onLogin }) => {
+  const [data, setData] = useState(CreateFormData);
 
-const Login = () => {
-    const[data,setData] = useState(CreateFormData)
+  const handleInputChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    const checked = event.target.checked;
+    const type = event.target.type;
 
- 
+    setData((data) => {
+      return {
+        ...data,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  };
 
-  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("login button pressed", data);
+    onLogin(data);
+  };
 
-    const handleInputChange = (event) => {
-        const name =event.target.name;
-        const value =event.target.value;
-        const checked =event.target.checked;
-        const type =event.target.type;
-        setData((data) =>{
-            return {
-                ...data,
-                [name]: type ===' checkbox' ? checked : value,
-            }
-        } )
-    }
-
-    return (
-        <form>
-        <input value={data.username} name="username" onInput={handleInputChange} placeholder="username"/>
-        <input value={data.password} type="password" name="password" onInput={handleInputChange} placeholder="password"/>
-        <input checked={data.remember} type="checkbox" name="remember" onChange={handleInputChange} />
+  return (
+    <>
+    <h2>Sign In</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={data.username}
+          name="username"
+          onChange={handleInputChange} 
+          placeholder="username"
+        />
+        <input
+          value={data.password}
+          type="password"
+          name="password"
+          onChange={handleInputChange} 
+          placeholder="password"
+        />
+        <input
+          checked={data.remember}
+          type="checkbox"
+          name="remember"
+          onChange={handleInputChange}
+        />
         <label htmlFor="remember">Remember me!</label>
-       
-        </form>
-    )
-}
+        <button type="submit" disabled={!data.username || !data.password}>
+          Login
+        </button>
+      </form>
+    </>
+  );
+};
 
 export default Login;
