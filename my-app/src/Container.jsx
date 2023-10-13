99cb989c20b7e9ba367351f9b4db82382ec71106
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-10-13 11:11:20 +0200

    container collapsable

--- my-app/src/Container.jsx
+++ my-app/src/Container.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import "./Container.css";
 
 const Container = ({ title, children }) => {
+  const [collapse, setCollapse] = useState(false);
+
+  const handleButtonToggle = () => {
+    setCollapse(!collapse);
+  };
+
   return (
     <div className="container">
-      <div className="container-title">{title}</div>
-      <div className="container-content">{children}</div>
+      <div className="container-title">
+        {title}
+        <button onClick={handleButtonToggle}>
+          {collapse ? "Close" : "Open"}
+        </button>
+      </div>
+      {collapse && <div className="container-content">{children}</div>}
     </div>
   );
 };
