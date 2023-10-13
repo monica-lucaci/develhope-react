f840a6b3bd7a5ef264bbb850a61547e7bee37603
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-10-13 22:40:43 +0200

    rendered counter at /counter

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,21 +1,23 @@
 import { Route, Routes } from "react-router-dom";
-import  Container  from "./Container";
+import Container from "./Container";
 import Welcome from "./Welcome";
-
+import Counter from "./Counter"; // Import the Counter component
 
 const App = () => {
   return (
     <Container>
       <Routes>
-        <Route path="/" element={<Welcome name="Monica" /> }/>
+        <Route path="/" element={<Welcome name="Monica" />} />
+        <Route
+          path="counter"
+          element={
+            <Counter initialValue={0} incrementAmount={1} decrementAmount={1} />
+          }
+        />
+        
       </Routes>
     </Container>
   );
 };
 
 export default App;
-
-
-
-//Render the App component within a BrowserRouter component.
-//did that in Root.jsx
